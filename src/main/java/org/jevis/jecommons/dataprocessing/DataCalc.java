0adb47a963932319dc1a0fa42ef2662c08f544ba
gonghao.feng@envidatec.com <>
2015-01-26 11:40:44 +0100

    change all imput parameter "JEVisAttribute" with "List<JEVisSample>" and "List<JEVisAttribute>" with "List<List<JEVisSample>>"

--- src/main/java/org/jevis/jecommons/dataprocessing/DataCalc.java
+++ src/main/java/org/jevis/jecommons/dataprocessing/DataCalc.java
@@ -18,133 +18,103 @@ import org.joda.time.DateTime;
 public interface DataCalc {
     
     /*
-     * @param attribute one sampled data row of typ JEVisAttribute to be calculated
+     * @param samples one sampled data row of typ JEVisAttribute to be calculated
      * @param value one value of typ double to be added
      * @return
      */
-    List<JEVisSample> addition(JEVisAttribute attribute, double value) throws JEVisException;
+    List<JEVisSample> addition(List<JEVisSample> samples, double value) throws JEVisException;
 
     /*
      * @param attributes the infinite many sampled data rows of typ JEVisAttribute to be calculated
      * @return
      */
-    public List<JEVisSample> addition(List<JEVisAttribute> attributes)throws JEVisException;
-    
-    /*
-     * @param attribute1 one sampled data row of typ JEVisAttribute to be calculated
-     * @param attribute2 one sampled data row of typ JEVisAttribute to be calculated
-     * @return
-     */
-    public List<JEVisSample> addition(JEVisAttribute attribute1, JEVisAttribute attribute2) throws JEVisException;
-                 
+    public List<JEVisSample> addition(List<List<JEVisSample>> attributes) throws JEVisException;
+                     
     /*
     the combination of high pass filter und low pass filter
     The input parameter „delete“ decides, whether the improper values 
     will be deleted or replaced by  the upper limit and  the lower limit.
-    * @param attribute one sampled data row of typ JEVisAttribute to be calculated
+    * @param samples one sampled data row of typ JEVisAttribute to be calculated
     * @param boundary_up the upper limit
     * @param boundary_low the lower limit
     * @param delete decides, whether the improper values will be deleted or replaced by  the upper limit and  the lower limit.(delete=true, delete; delete=false, replace)
     * @return
     */
-    public List<JEVisSample> boundaryFilter(JEVisAttribute attribute, double boundary_up, double boundary_low, boolean delete) throws JEVisException;
+    public List<JEVisSample> boundaryFilter(List<JEVisSample> samples, double boundary_up, double boundary_low, boolean delete) throws JEVisException;
 
     /*
-    * @param attribute one sampled data row of typ JEVisAttribute to be calculated
+    * @param samples one sampled data row of typ JEVisAttribute to be calculated
     * @return
     */
-    public List<JEVisSample> cumulativeDifferentialConverter(JEVisAttribute sample) throws JEVisException;
+    public List<JEVisSample> cumulativeDifferentialConverter(List<JEVisSample> samples) throws JEVisException;
 
     /*
      * 1. the over boundary value will not be stored, and become a gap.  no
      * 2. the over boundary value will replaced by boundary.
      * 3. the over boundary value will replaced by 0 or any other value.
      * here choose 2
-     * @param attribute one sampled data row of typ JEVisAttribute to be calculated
+     * @param samples one sampled data row of typ JEVisAttribute to be calculated
      * @param boundary the upper limit
      * @return
      */
-    public List<JEVisSample> highPassFilter(JEVisAttribute attribute, double boundary) throws JEVisException;
+    public List<JEVisSample> highPassFilter(List<JEVisSample> samples, double boundary) throws JEVisException;
 
     /*
      * 1. the over boundary value will not be stored, and become a gap.  no
      * 2. the over boundary value will replaced by boundary.
      * 3. the over boundary value will replaced by 0 or any other value.
      * here choose 3
-     * @param attribute one sampled data row of typ JEVisAttribute to be calculated
+     * @param samples one sampled data row of typ JEVisAttribute to be calculated
      * @param boundary the upper limit
      * @param fill_value the value,that replaces the inproper values 
      * @return
      */
-    public List<JEVisSample> highPassFilter(JEVisAttribute attribute, double boundary, double fill_value) throws JEVisException;
+    public List<JEVisSample> highPassFilter(List<JEVisSample> samples, double boundary, double fill_value) throws JEVisException;
 
     /*
      * calculate the area between every two time points,and here the period
      * between every two time points must not be same.
      * And there could be sevrial methods to calculate the integration:vorwaerts,nachwaerts,trapezoid, 
      * here trapezoid is used.
-     * @param attribute one sampled data row of typ JEVisAttribute to be calculated
+     * @param samples one sampled data row of typ JEVisAttribute to be calculated
      * @return
      */
-    public double integration(JEVisAttribute attribute) throws JEVisException;
+    public double integration(List<JEVisSample> samples) throws JEVisException;
     
-    /*
-     * calculate the area between every two time points,and here the period
-     * between every two time points must not be same.
-     * And there could be sevrial methods to calculate the integration:vorwaerts,nachwaerts,trapezoid, 
-     * here trapezoid is used.
-     * calculate in a range
-     * @param attribute one sampled data row of typ JEVisAttribute to be calculated
-     * @param from the begintime
-     * @param to the endtime.
-     * @return
-     */
-    public double integration(JEVisAttribute attribute, DateTime from, DateTime to) throws JEVisException;
-
     /*
      * eliminate the deviation of the timestamp(delay). 
      * the smallest unit of time,period_s and deviation_s is second
-     * @param attribute one sampled data row of typ JEVisAttribute to be calculated
+     * @param samples one sampled data row of typ JEVisAttribute to be calculated
      * @param period_s period of the sampling and it's units is second. 
      * @param deviation_s allowable deviation time and it's units second. 
      * @return
      */
-    public List<JEVisSample> intervalAlignment(JEVisAttribute attribute, DateTime begin_time, int period_s, int deviation_s) throws JEVisException;
+    public List<JEVisSample> intervalAlignment(List<JEVisSample> samples, DateTime begin_time, int period_s, int deviation_s) throws JEVisException;
 
     /*
      * interpolation the whole data row, from begin to end
-     * @param attribute one sampled data row of typ JEVisAttribute to be calculated
+     * @param samples one sampled data row of typ JEVisAttribute to be calculated
      * @param insert_num decides,how many points will be added into every two time points in original data row.
      * @return
      */
-    public List<JEVisSample> linearInterpolation(JEVisAttribute attribute, int insert_num) throws JEVisException;
-    
-    /*
-     * interpolation in a range
-     * @param attribute one sampled data row of typ JEVisAttribute to be calculated
-     * @param from the begintime
-     * @param to the endtime
-     * @param insert_num decides,how many points will be added into every two time points in original data row.
-     * @return
-     */
-    public List<JEVisSample> linearInterpolation(JEVisAttribute attribute, DateTime from, DateTime to, int insert_num) throws JEVisException;
+    public List<JEVisSample> linearInterpolation(List<JEVisSample> samples, int insert_num) throws JEVisException;
 
     /*
      * calculate every value according to y=a*x+b
      * the gap is not taken into considered until now
-     * @param attribute one sampled data row of typ JEVisAttribute to be calculated
+     * @param samples one sampled data row of typ JEVisAttribute to be calculated
      * @param proportion 
      * @param b
      * @return
      */
-    public List<JEVisSample> linearScaling(JEVisAttribute attribute, double proportion, double b) throws JEVisException;
+    public List<JEVisSample> linearScaling(List<JEVisSample> samples, double proportion, double b) throws JEVisException;
     
     /*
     look for the median of one data row(JEVis variable). 
-    * @param attribute one sampled data row of typ JEVisAttribute to be calculated
+    * @param samples one sampled data row of typ JEVisAttribute to be calculated
     * @return
     */
-    public double median(JEVisAttribute attribute) throws JEVisException;
+    public double median(List<JEVisSample> samples) throws JEVisException;
 
     /*
      * merge the values at some timestamps to one timestamp, the values will be added. 
@@ -152,83 +122,83 @@ public interface DataCalc {
      * The input parameter „period_s“ should be time(period) and it's unit is  second. 
      * The last input parameter „meg_num“ means, how many sampled value will be merged.
      * (millinsecond for year is too lang,already beyound the int,so the smallest unit of time is here second.)
-     * @param attribute one sampled data row of typ JEVisAttribute to be calculated
+     * @param samples one sampled data row of typ JEVisAttribute to be calculated
      * @param begin_time the begintime
      * @param period_s period of the sampling and it's units is second. 
      * @param meg_num means, how many sampled value will be merged.
      * @return
      */
-    public List<JEVisSample> mergeValues(JEVisAttribute attribute, DateTime begin_time, int period_s, int meg_num) throws JEVisException;
+    public List<JEVisSample> mergeValues(List<JEVisSample> samples, DateTime begin_time, int period_s, int meg_num) throws JEVisException;
 
     /*
     delete the value,that is not bigger or smaller than it's previous value in one percentage value. 
     The inputparameter „percent“ is the percentage value, which is decided by enduser.
-    * @param attribute one sampled data row of typ JEVisAttribute to be calculated
+    * @param samples one sampled data row of typ JEVisAttribute to be calculated
     * @param percent the percentage value
     * @return
     */
-    public List<JEVisSample> precisionFilter(JEVisAttribute attribute, double percent) throws JEVisException;
+    public List<JEVisSample> precisionFilter(List<JEVisSample> samples, double percent) throws JEVisException;
 
     /*
     sort the data row according to Time. The import parameter "order" decides form begin/end to end/begin.
     order= 1,begin to end
     order=-1,end to begin
-    * @param attribute one sampled data row of typ JEVisAttribute to be calculated
+    * @param samples one sampled data row of typ JEVisAttribute to be calculated
     * @param order decides,whether is from begen to end(1) or from end to begin(-1). The value of „order“ should be 1 or -1.
     * @return
     */
-    public List<JEVisSample> sortByTime(JEVisAttribute attribute,int order);
+    public List<JEVisSample> sortByTime(List<JEVisSample> samples, int order);
     
     /*
     sort the data row according to value. The import parameter "order" decides form big/small to small/big.
     order= 1,small to big
     order=-1,big to small
-    * @param attribute one sampled data row of typ JEVisAttribute to be calculated
+    * @param samples one sampled data row of typ JEVisAttribute to be calculated
     * @param order decides,whether is from big to small(1) or from samll to big(-1). 
     * @return
     */
-    public List<JEVisSample> sortByValue(JEVisAttribute attribute,int order);
+    public List<JEVisSample> sortByValue(List<JEVisSample> samples, int order);
     
     /*
      * only split the value as average, it's not komplete
-     * @param attribute one sampled data row of typ JEVisAttribute to be calculated
+     * @param samples one sampled data row of typ JEVisAttribute to be calculated
      * @param period_s period of the sampling and it's units is second.
      * @param seg_num means, how many time points will one sampled time point will be splitted into.(seg_num=1,one point becomes two points; seg_num=2,one point becomes three points; ......)
      * @return
      */
-    public List<JEVisSample> splitValues(JEVisAttribute attribute, int period_s, int seg_num) throws JEVisException;
+    public List<JEVisSample> splitValues(List<JEVisSample> samples, int period_s, int seg_num) throws JEVisException;
     
     /*
     every value of the data row minus one value
-    * @param attribute one sampled data row of typ JEVisAttribute to be calculated
+    * @param samples one sampled data row of typ JEVisAttribute to be calculated
     * @param value one value of typ double to be subtracted
     * @return
     */
-    public List<JEVisSample> subtraction(JEVisAttribute attribute, double value) throws JEVisException;
+    public List<JEVisSample> subtraction(List<JEVisSample> samples, double value) throws JEVisException;
 
     /*
     the value in first data row minus the value in second data row with same time.
     the time punkts,that only the first data row has, it's value will be directly putted into the result.
     the time punkts,that only the second data row has, it's value will become negative and be putted into the result.
-    * @param attribute1 one sampled data row of typ JEVisAttribute to subtract
-    * @param attribute2 one sampled data row of typ JEVisAttribute to be subtracted
+    * @param samples1 one sampled data row of typ JEVisAttribute to subtract
+    * @param samples2 one sampled data row of typ JEVisAttribute to be subtracted
     * @return
     */
-    public List<JEVisSample> subtraction(JEVisAttribute attribute1, JEVisAttribute attribute2) throws JEVisException;
+    public List<JEVisSample> subtraction(List<JEVisSample> samples1, List<JEVisSample> samples2) throws JEVisException;
 
     /*
      * output all minimum values with their time in the data row
-     * @param attribute one sampled data row of typ JEVisAttribute to be processed
+     * @param samples one sampled data row of typ JEVisAttribute to be processed
      * @return
      */
-    public List<JEVisSample> valueAllMinimum(JEVisAttribute attribute) throws JEVisException;
+    public List<JEVisSample> valueAllMinimum(List<JEVisSample> samples) throws JEVisException;
 
     /*
      * output only the minimum value in one data row
-     * @param attribute one sampled data row of typ JEVisAttribute to be processed
+     * @param samples one sampled data row of typ JEVisAttribute to be processed
      * @return
      */
-    public double valueMinimum(JEVisAttribute attribute) throws JEVisException;
+    public double valueMinimum(List<JEVisSample> samples) throws JEVisException;
 
     
     /*
@@ -238,7 +208,7 @@ public interface DataCalc {
     * @param attributes infinite many sampled data rows of typ JEVisAttribute to be processed
     * @return
     */
-    public double valueMinimum(List<JEVisAttribute> attributes) throws JEVisException;
+    public double valueMinimumMore(List<List<JEVisSample>> attributes) throws JEVisException;
 
     /*
     find the minimum value of multiple data rows and a value. 
@@ -248,7 +218,7 @@ public interface DataCalc {
     * @param value one value of typ double to be compared
     * @return
     */
-    public double valueMinimum(List<JEVisAttribute> attributes,double value) throws JEVisException;
+    public double valueMinimumMore(List<List<JEVisSample>> attributes,double value) throws JEVisException;
     
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     public List<JEVisSample> multiplication(JEVisAttribute f1, JEVisAttribute f2) throws ParseException, JEVisException;
